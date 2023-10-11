70db6ff580d4a04767a3febfcd38e0bab762fc30
Connor Slade <connor@connorcode.com>
2023-10-10 23:27:22 -0400

    Support descriptions and links on units

--- unit-converter/wasm.js
+++ unit-converter/wasm.js
@@ -175,6 +175,25 @@ function getInt32Memory0() {
     }
     return cachedInt32Memory0;
 }
+/**
+* @returns {string}
+*/
+export function version() {
+    let deferred1_0;
+    let deferred1_1;
+    try {
+        const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
+        wasm.version(retptr);
+        var r0 = getInt32Memory0()[retptr / 4 + 0];
+        var r1 = getInt32Memory0()[retptr / 4 + 1];
+        deferred1_0 = r0;
+        deferred1_1 = r1;
+        return getStringFromWasm0(r0, r1);
+    } finally {
+        wasm.__wbindgen_add_to_stack_pointer(16);
+        wasm.__wbindgen_free(deferred1_0, deferred1_1, 1);
+    }
+}
 
 let cachedFloat64Memory0 = null;
 
