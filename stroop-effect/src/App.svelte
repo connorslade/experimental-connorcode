edbda599b4b3212dc1644dfefac5446017a52720
Connor Slade <connor@connorcode.com>
2024-10-30 16:07:18 -0400

    Working basics of stroop-effect

---
+++ stroop-effect/src/App.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { random } from "./colors";
+
+  let random_colors = true;
+
+  let current = random();
+  let timestamp = new Date();
+
+  document.addEventListener("keydown", (e) => {
+    if (e.code !== "Space") return;
+
+    let now = new Date();
+    let delta = now.getTime() - timestamp.getTime();
+    timestamp = now;
+
+    let last_name = current.name;
+    while (current.name === last_name) current = random();
+    e.preventDefault();
+  });
+</script>
+
+<main>
+  <div class="container">
+    <p style:color={random_colors ? current.color : "black"} class="name">
+      {current.name}
+    </p>
+  </div>
+
+  <input type="checkbox" id="a" bind:checked={random_colors} />
+  <label for="a">Random Colors</label>
+</main>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .name {
+    font-size: 200px;
+    font-family: sans-serif;
+  }
+</style>
--- stroop-effect/src/colors.ts
+++ stroop-effect/src/colors.ts
@@ -8,10 +8,22 @@ class Color {
   }
 }
 
-const colors = [
-  new Color("red", "Red"),
-  new Color("green", "Green"),
-  new Color("blue", "Blue"),
+export const colors = [
+  new Color("#dc2626", "Red"),
+  new Color("#ea580c", "Orange"),
+  new Color("#facc15", "Yellow"),
+  new Color("#16a34a", "Green"),
+  new Color("#1d4ed8", "Blue"),
+  new Color("#5b21b6", "Purple"),
 ];
 
-export default colors;
+function random_color() {
+  let idx = Math.floor(Math.random() * colors.length);
+  return colors[idx];
+}
+
+export function random() {
+  let name = random_color().name;
+  let color = random_color().color;
+  return new Color(color, name);
+}
